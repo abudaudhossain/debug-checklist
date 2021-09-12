2b8fb9d2c2978bf218340303f08247ab58ed675e
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-12 08:42:47 +0600

    add pattern

--- email_valid/valid.js
+++ email_valid/valid.js
@@ -1,6 +1,6 @@
 const valid = document.getElementById('valid');
 const unValid = document.getElementById("unValid");
-
+const emailFeild = document.getElementById('email-field');
 
 const messageDisplay = (id, isdisplay) =>{
     if(isdisplay){
@@ -10,11 +10,13 @@ const messageDisplay = (id, isdisplay) =>{
         id.classList.remove("d-none");
     }
 }
+const pattern = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
 
 document.getElementById('submit-btn').addEventListener("click", ()=>{
-    const emailFeild = document.getElementById('email-field');
+    // const emailFeild = document.getElementById('email-field');
     console.log(emailFeild);
-    if(emailFeild.value > 5){
+    if(pattern.test(emailFeild.value)){
         messageDisplay(valid, false);
         messageDisplay(unValid, true);
     }else{
